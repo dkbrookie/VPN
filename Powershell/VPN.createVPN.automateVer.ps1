869bdfc65b28f7430cbdb98eff7f05fb54c616fb
dkbrookie <matthew@dkbinnovative.net>
2019-04-17 15:19:18 -0500

    Update VPN.createVPN.automateVer.ps1

--- Powershell/VPN.createVPN.automateVer.ps1
+++ Powershell/VPN.createVPN.automateVer.ps1
@@ -2,25 +2,20 @@ If (!($serverAddress)) {
   Write-Warning "!ERROR: No server address defined, exiting script"
   Break
 }
-
 If (!($connectionName)) {
   Write-Warning "!ERROR: No connection name was defined, exiting script"
   Break
 }
-
 If (!($presharedKey)) {
   Write-Warning "!ERROR: No preshare key was defined, exiting script"
   Break
 }
-
 If (!($tunnelType)) {
   Write-Warning "!ERROR: No tunnel type was defined, exiting script"
   Break
 }
-
 If (!($authenticationMethod)) {
   Write-Warning "!ERROR: No authentication method was defined, exiting script"
   Break
 }
-
 Add-VpnConnection -Name $connectionName -ServerAddress $serverAddress -TunnelType $tunnelType -AllUserConnection -L2tpPsk $presharedKey -AuthenticationMethod $authenticationMethod -Force
