4819dcfbedfbef05383bc7fd43fad067c5de71af
dkbrookie <42004970+dkbrookie@users.noreply.github.com>
2019-08-02 08:54:00 -0500

    Update VPN.Automate.Monitor.Check.If.VPN.Exists.ps1
    -Changed it so this not only checks for the VPN connection but also creates it if it doesn't exist using variables passed in from an Automate remote monitor

--- Powershell/VPN.Automate.Monitor.Check.If.VPN.Exists.ps1
+++ Powershell/VPN.Automate.Monitor.Check.If.VPN.Exists.ps1
@@ -1,8 +1,29 @@
-$vpnName = "$clientName VPN" #-replace ('e:','')
-#$vpnName = "$vpnName VPN"
+## $clientName is passed in from the monitor. It pulls this value from the ClientName
+## EDF located at the Client level.
+$vpnName = "$clientName VPN"
+## See if the VPN connection already exists
 $vpnPresent = Get-VpnConnection -AllUserConnection -Name $vpnName
 If (!$vpnPresent) {
-    Write-Warning "!ERROR: $vpnName does not exist!"
+    Try {
+        ## Create the VPN connection
+        Write-Warning "$vpnName does not exist, creating connection..."
+        Add-VpnConnection -Name $vpnName -ServerAddress $serverAddress -TunnelType $tunnelType -AllUserConnection -L2tpPsk $presharedKey -AuthenticationMethod $authenticationMethod -Force
+        ## Check for the VPN connection again to see if it exists now
+        $vpnPresent = Get-VpnConnection -AllUserConnection -Name $vpnName
+        If ($vpnPresent) {
+            Write-Output "!SUCCESS: Created $vpnName successfully"
+            Break
+        } Else {
+            Write-Warning "!ERROR: Failed to created $vpnName"
+            Break
+        }
+    } Catch {
+        ## If there was an error thrown during VPN connection creation it will come here and put out this error
+        Write-Warning "!ERROR: There was a problem when attempting to create $vpnName"
+        Break
+    }
 } Else {
-    Write-Output "Verified $vpnName exists"
+    ## If the tunnel already exists the script goes straight to here and exits
+    Write-Output "!SUCCESS: Verified $vpnName exists"
+    Break
 }
